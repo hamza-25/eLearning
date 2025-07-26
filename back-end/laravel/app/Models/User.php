934eb2b5e97684d438148b1f308cec3bb85a510b
hamza-25 <me677499@gmail.com>
2025-07-26 11:38:24 +0100

    test method login and register, it return token and add api.php file

--- back-end/laravel/app/Models/User.php
+++ back-end/laravel/app/Models/User.php
@@ -65,6 +65,8 @@ class User extends Authenticatable implements JWTSubject
 
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'role' => $this->role,
+        ];
     }
 }
